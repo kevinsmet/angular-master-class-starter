995dbe7ab536b23c4aedbf7ae4ee0915c9553be3
Kevin Smet <kevin.smet@cegeka.com>
2018-07-10 11:27:43 +0200

    day2 - excercise 2

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ContactsService} from '../contacts.service';
 import {Observable} from 'rxjs/internal/Observable';
 import {Contact} from '../models/contact';
+import {Subject} from 'rxjs/internal/Subject';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -9,11 +11,14 @@ import {Contact} from '../models/contact';
 })
 export class ContactsListComponent implements OnInit {
   contacts$: Observable<Array<Contact>>;
+  terms$ = new Subject<string>();
 
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit() {
     this.contacts$ = this.contactsService.getContacts();
+    this.terms$.pipe(debounceTime(400),
+      distinctUntilChanged()).subscribe(term => this.search(term));
   }
 
   search(term: string) {
